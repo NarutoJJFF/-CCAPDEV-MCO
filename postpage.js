e9c6fe62b9f3fba95ccf9f1892d29e8d8c3bbc30
retroduck385 <retroduck385@gmail.com>
2025-02-06 12:27:14 +0800

    Postpage

---
+++ postpage.js
@@ -0,0 +1,20 @@
+function changeImageLike(){
+    let displayImage = document.getElementById('like');
+    let filename = displayImage.src.split('/').pop(); 
+    if (filename === 'heart.png') {
+        displayImage.src = 'Header Elements/red-heart.png';
+    } else {
+        displayImage.src = 'Header Elements/heart.png';
+    }
+}
+
+function changeImageDislike(){
+    let displayImage = document.getElementById('dislike');
+    let filename = displayImage.src.split('/').pop();
+
+    if (filename === 'broken-heart.png') {
+        displayImage.src = 'Header Elements/red-broken-heart.png';
+    } else {
+        displayImage.src = 'Header Elements/broken-heart.png';
+    }
+}
