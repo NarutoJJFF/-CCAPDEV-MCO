511a43415ad0286f42172880d7121dcce6697187
retroduck385 <retroduck385@gmail.com>
2025-03-07 03:34:37 +0800

    tried to start server

---
+++ server.js
@@ -0,0 +1,6 @@
+const exxpress = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+moongose.connect('mongodb+srv://josh:dbjoshpassword@apdevmc.vfohc.mongodb.net/')
