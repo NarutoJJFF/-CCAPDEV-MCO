e4b64f18f72bd7ea8b0e0eb57f4a901d98865496
retroduck385 <retroduck385@gmail.com>
2025-03-08 16:17:03 +0800

    done stuff

--- app.js
+++ app.js
@@ -1,6 +1,56 @@
-const exxpress = require('express')
-const mongoose = require('mongoose')
+const express = require('express');
+const server = express();
 
-const app = express()
+const bodyParser = require('body-parser');
+server.use(express.json()); 
+server.use(express.urlencoded({ extended: true }));
 
-moongose.connect('mongodb+srv://josh:dbjoshpassword@apdevmc.vfohc.mongodb.net/')
+const handlebars = require('express-handlebars');
+server.set('view engine', 'hbs');
+server.engine('hbs', handlebars.engine({
+  extname: 'hbs',
+}));
+
+server.use(express.static('public'));
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://josh:dbjoshpassword@apdevmc.vfohc.mongodb.net/?retryWrites=true&w=majority&appName=APDEVMC');
+
+
+const postSchema = new mongoose.Schema({
+    tag: { type: String , required: true},
+    title: {type: String , required: true},
+    accID: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},  //Subject to change depends on the user database
+    content:{type: String , required: true},
+    votes: { 
+        upvotes: { type: Number, default: 0 }, 
+        downvotes: { type: Number, default: 0 }
+    }
+
+  },{versionKey: false, timestamps: true});
+
+const Post = mongoose.model('Post', postSchema);
+
+const commentSchema = new mongoose.Schema({
+    postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true }, // Links to Post
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Links to User
+    content: { type: String, required: true },
+    parentComment: { type: mongoose.Schema.Types.ObjectId, ref: 'Comment', default: null }, // For nested comments
+    votes: { 
+        upvotes: { type: Number, default: 0 }, 
+        downvotes: { type: Number, default: 0 }
+    }
+
+}, {versionKey: false, timestamps: true});
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+
+server.get('/', function(req, resp){
+    resp.render('homepage',{
+      layout: 'homepageLayout',
+      title: 'Home page'
+    });
+  });
+
+server.listen(3000, () => console.log('Server running on http://localhost:3000'));
