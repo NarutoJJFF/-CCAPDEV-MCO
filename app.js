e3b80a44c01941212058350568e4745bb1d5b151
Nerdico36 <golsgh@gmail.com>
2025-03-10 00:17:47 +0800

    converted login,register pages with db, polished header

--- app.js
+++ app.js
@@ -24,6 +24,14 @@ try{
     console.log("Error MongoDB not connected")
 }
 
+const userSchema = new mongoose.Schema({
+  username: { type: String , required: true},
+  password: { type: String , required: true},
+  profileImg: { type: String , default: null},
+  bio: { type: String, default: ""}
+},{versionKey: false, timestamps: true});
+
+const User = mongoose.model('User', userSchema);
 
 const postSchema = new mongoose.Schema({
     tag: { type: String , required: true},
@@ -93,9 +101,6 @@ server.get('/', async function (req, resp) {
   }
 });
 
-server.listen(3000, () => console.log('Server running on http://localhost:3000'));
-
-
 server.get('/add-post-page', async function(req,resp){
   resp.render('addPost',{
     layout: 'addPostLayout',
@@ -126,3 +131,131 @@ server.post('/add-post', async function(req, resp) {
     });
   }
 });
+
+server.get('/login-page', async function(req,resp){
+  resp.render('login',{
+    layout: 'loginRegisterLayout',
+    title: 'Login Page',
+    failed: false
+  });
+});
+
+server.post('/login', async function(req, resp) {
+  const searchQuery = { username: req.body.username, password: req.body.password };
+  let login = await User.findOne(searchQuery);
+  console.log('Finding user');
+
+  if(login != undefined && login._id != null){
+    let postResult = await Post.find({}); 
+    const plainPosts = postResult.map(post => post.toObject());
+    resp.render('homepage', { 
+      layout: 'homepageLayout',
+      title: 'Home page',
+      posts: plainPosts, 
+    });
+  }else{
+    resp.render('login',{
+      layout: 'loginRegisterLayout',
+      title:  'Login Page',
+      failed: true });
+  }
+});
+
+server.get('/register-page', async function(req,resp){
+  resp.render('register',{
+    layout: 'loginRegisterLayout',
+    title: 'Registration Page',
+    passw_error: false,
+    uname_taken: false,
+    db_error: false
+  });
+});
+
+server.post('/register', async function(req, resp) {
+  if(req.body.password != req.body.conf_password){
+
+    resp.render('register',{
+      layout: 'loginRegisterLayout',
+      title:  'Registration Page',
+      passw_error: true,
+      uname_taken: false,
+      db_error: false
+    });
+
+  }else{
+
+    const searchQuery = { username: req.body.username };
+    let login = await User.findOne(searchQuery);
+    console.log('Finding user/s');
+
+    if(login != undefined && login._id != null){
+      resp.render('register',{
+        layout: 'loginRegisterLayout',
+        title:  'Registration Page',
+        passw_error: false,
+        uname_taken: true,
+        db_error: false
+      });
+
+    }else{
+
+      try {
+        const newUser = new User({
+          username: req.body.username,
+          password: req.body.password,
+          profileImg: null,
+          bio: "New user"
+        });
+    
+        await newUser.save();
+        console.log('User created successfully');
+    
+        resp.redirect('/');
+        
+      } catch (error) {
+
+        console.error('Error creating post:', error);
+
+        resp.status(500).render('register', {
+          layout: 'loginRegisterLayout',
+          title: 'Registration Page',
+          passw_error: false,
+          uname_taken: false,
+          db_error: true
+        });
+
+      }
+    }
+  }
+
+  const searchQuery = { username: req.body.username, password: req.body.password };
+  let login = await User.findOne(searchQuery);
+  console.log('Finding user');
+
+  if(login != undefined && login._id != null){
+    let postResult = await Post.find({}); 
+    const plainPosts = postResult.map(post => post.toObject());
+    resp.render('homepage', { 
+      layout: 'homepageLayout',
+      title: 'Home page',
+      posts: plainPosts, 
+    });
+  }else{
+    resp.render('login',{
+      layout: 'loginRegisterLayout',
+      title:  'Login Page',
+      failed: true });
+  }
+});
+
+function finalClose(){
+  console.log('Close connection at the end!');
+  mongoose.connection.close();
+  process.exit();
+}
+
+process.on('SIGTERM',finalClose);  //general termination signal
+process.on('SIGINT',finalClose);   //catches when ctrl + c is used
+process.on('SIGQUIT', finalClose); //catches other termination commands
+
+server.listen(3000, () => console.log('Server running on http://localhost:3000'));
