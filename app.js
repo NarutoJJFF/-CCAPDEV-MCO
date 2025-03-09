84ef4a4b0d494544ddd0976523080b75e6806c5c
retroduck385 <retroduck385@gmail.com>
2025-03-09 16:33:48 +0800

    .

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ try{
 const postSchema = new mongoose.Schema({
     tag: { type: String , required: true},
     title: {type: String , required: true},
-    accID: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},  //Subject to change depends on the user database
+    //accID: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},  //Subject to change depends on the user database
     content:{type: String , required: true},
     upvotes: { type: Number, default: 0 }, 
     downvotes: { type: Number, default: 0 }
