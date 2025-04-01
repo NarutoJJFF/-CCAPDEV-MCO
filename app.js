31de38961c127f67e972ee77cecd7bfc227f830b
NarutoJJFF <joshua_fabiana@yahoo.com>
2025-04-02 02:13:48 +0800

    [FIXED] Homepage Profile Heads Redirection

--- app.js
+++ app.js
@@ -9,10 +9,14 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
 const handlebars = require('express-handlebars');
+const hbs = handlebars.create({
+    extname: 'hbs',
+    helpers: {
+        eq: (a, b) => a.toString() === b.toString()
+    }
+});
+server.engine('hbs', hbs.engine);
 server.set('view engine', 'hbs');
-server.engine('hbs', handlebars.engine({
-  extname: 'hbs',
-}));
 
 server.use(express.static('public'));
 
