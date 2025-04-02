ad642fedbde78787717a2d77fbeda148ef2161e1
retroduck385 <retroduck385@gmail.com>
2025-04-03 00:34:01 +0800

    Fixing Nested Comments

--- model/comment.js
+++ model/comment.js
@@ -4,7 +4,7 @@ const commentSchema = new mongoose.Schema({
     postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post'}, // Links to Post
     author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Links to User
     content: { type: String, required: true },
-    parentComment: { type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}, // For nested comments
+    parentComment: { type: mongoose.Schema.Types.ObjectId, ref: 'Comment', default: null }, // For nested comments
     upvoteCount: {type: Number, default: 0},
     downvoteCount: {type: Number, default: 0},
     upvotes: [{type: mongoose.Schema.Types.String, ref: 'User'}],    //i'll just use .length to find the amount
